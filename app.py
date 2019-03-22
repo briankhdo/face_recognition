8988a13777474c9ffaaaf808e52ebc8d393278eb
Brian Doan <quangkhoidoan@gmail.com>
2019-03-22 11:05:23 +0700

    Remove GPU

--- app.py
+++ app.py
@@ -69,19 +69,18 @@ def read_tensor_from_image_data(image_data,
                                 input_width=299,
                                 input_mean=0,
                                 input_std=255):
-    with tf.device('/gpu:0'):
-        input_name = "file_reader"
-        output_name = "normalized"
-        image_reader = tf.image.decode_jpeg(
-                image_data, channels=3, name="jpeg_reader")
-        float_caster = tf.cast(image_reader, tf.float32)
-        dims_expander = tf.expand_dims(float_caster, 0)
-        resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
-        normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-        sess = tf.Session()
-        result = sess.run(normalized)
+    input_name = "file_reader"
+    output_name = "normalized"
+    image_reader = tf.image.decode_jpeg(
+            image_data, channels=3, name="jpeg_reader")
+    float_caster = tf.cast(image_reader, tf.float32)
+    dims_expander = tf.expand_dims(float_caster, 0)
+    resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+    normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+    sess = tf.Session()
+    result = sess.run(normalized)
 
-        return result
+    return result
 
 
 def load_labels(label_file):
